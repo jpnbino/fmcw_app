294126d3bc2067a023be6a6e27fc8ebf55b85dcc
Joao Paulo Bino <joao.nogueirabino@edu.fh-joanneum.at>
2024-06-28 00:04:08 +0200

    Fixed labels in self.show_status_bit section refactored write_bms_config

--- src/gui/bms.py
+++ src/gui/bms.py
@@ -2,6 +2,8 @@ from PyQt5.QtGui import QColor
 from bms.constants import *
 from serialbsp.protocol import *
 
+import logging
+
 class BMSGUI:
     def __init__(self, ui, bms_config):
         self.ui = ui
@@ -10,21 +12,6 @@ class BMSGUI:
         # Connect button click to custom functions
         self.ui.readPackButton.clicked.connect(self.read_bms_config)
         self.ui.writeEepromButton.clicked.connect(self.write_bms_config)
-
-        # Connect QLineEdit signals to a common slot
-        for line_edit in [
-            self.ui.ovLockoutLineEdit,
-            self.ui.ovLineEdit,
-            self.ui.ovRecoverLineEdit,
-            self.ui.eocVoltageLineEdit,
-            self.ui.uvRecoverLineEdit,
-            self.ui.underVoltageLineEdit,
-            self.ui.sleepVoltageLineEdit,
-            self.ui.LowVoltageChargeLineEdit,
-            self.ui.uvLockoutLineEdit,
-            self.ui.ovDelayTimeoutLineEdit,
-        ]:
-            line_edit.textChanged.connect(self.on_line_edit_changed)
 
     def get_unit_from_combo(self, combo):
         """Read the combobox and return the unit chosen by user."""
@@ -238,14 +225,15 @@ class BMSGUI:
         
     def update_status_bits(self):
         #Status Bits from addresses 0x80, 0x81, 0x82, 0x83
+        #address 0x80
         self.show_status_bit(self.ui.bitOVlabel,    self.bms_config.bit_ov)
         self.show_status_bit(self.ui.bitOVLOlabel,  self.bms_config.bit_ovlo)
-        self.show_status_bit(self.ui.bitEOCHGlabel, self.bms_config.bit_uv)
-        self.show_status_bit(self.ui.bitOVlabel,    self.bms_config.bit_uvlo)
-        self.show_status_bit(self.ui.bitOVLOlabel,  self.bms_config.bit_dot)
-        self.show_status_bit(self.ui.bitEOCHGlabel, self.bms_config.bit_dut)        
-        self.show_status_bit(self.ui.bitOVlabel,    self.bms_config.bit_cot)
-        self.show_status_bit(self.ui.bitOVlabel,    self.bms_config.bit_cut)
+        self.show_status_bit(self.ui.bitUVlabel, self.bms_config.bit_uv)
+        self.show_status_bit(self.ui.bitUVLOlabel,    self.bms_config.bit_uvlo)
+        self.show_status_bit(self.ui.bitDOTlabel,  self.bms_config.bit_dot)
+        self.show_status_bit(self.ui.bitDUTlabel, self.bms_config.bit_dut)        
+        self.show_status_bit(self.ui.bitCOTlabel,    self.bms_config.bit_cot)
+        self.show_status_bit(self.ui.bitCUTlabel,    self.bms_config.bit_cut)
         
         #address 0x81
         self.show_status_bit(self.ui.bitIOTlabel,   self.bms_config.bit_iot)
@@ -261,9 +249,9 @@ class BMSGUI:
         self.show_status_bit(self.ui.bitCHPRSNTlabel, self.bms_config.bit_ch_prsnt)
         self.show_status_bit(self.ui.bitCHINGlabel,   self.bms_config.bit_ching)
         self.show_status_bit(self.ui.bitDCHINGlabel,  self.bms_config.bit_dching)
-        #self.show_status_bit(self.ui.bitlabel,  self.bms_config.bit_ecc_used)
-        #self.show_status_bit(self.ui.bitlabel, self.bms_config.bit_ecc_fail)        
-        #self.show_status_bit(self.ui.bitlabel,    self.bms_config.bit_int_scan)
+        #bit 4 not implemented
+        #bit 5 not implemented
+        #bit 6 not implemented
         self.show_status_bit(self.ui.bitLVCHRGlabel,    self.bms_config.bit_lvchg)        
         
         #address 0x83
@@ -287,90 +275,77 @@ class BMSGUI:
         current_style = label.styleSheet()
         label.setStyleSheet(f"{current_style} background-color: {color.name()};")
 
-    def write_bms_config(self):
 
-        register_cfg = self.bms_config.get_config()
+    def convert_to_hex(self, value):
+        try:
+            return int(float(value) / VOLTAGE_CELL_MULTIPLIER)
+        except ValueError:
+            print(f"Error converting {value} to hex.")
+            return 0
+    
+    def write_voltage_registers(self):
+        voltage_values = [
+            (self.ui.ovLineEdit.text(),     0x00),
+            (self.ui.ovRecoverLineEdit.text(), 0x02),
+            (self.ui.underVoltageLineEdit.text(), 0x04),
+            (self.ui.uvRecoverLineEdit.text(), 0x06),
+            (self.ui.ovLockoutLineEdit.text(), 0x08),
+            (self.ui.uvLockoutLineEdit.text(), 0x0a),
+            (self.ui.eocVoltageLineEdit.text(), 0x0c),
+            (self.ui.LowVoltageChargeLineEdit.text(), 0x0e),
+            (self.ui.sleepVoltageLineEdit.text(), 0x44)
+        ]
+        for value, address in voltage_values:
+            hex_value = self.convert_to_hex(value)
+            self.bms_config.reg_write(address, hex_value, MASK_12BIT, 0x00)
 
-        print("entered write_bms_config:", register_cfg)
-
-        # Convert Editline values to HEX
-        # Get the values from the QLineEdit fields
-        ov = float(self.ui.ovLineEdit.text()) #reg00
-        ov_recover = float(self.ui.ovRecoverLineEdit.text())
-        
-        uv = float(self.ui.underVoltageLineEdit.text())        
-        uv_recover = float(self.ui.uvRecoverLineEdit.text())
-
-        ov_lockout = float(self.ui.ovLockoutLineEdit.text())
-        uv_lockout = float(self.ui.uvLockoutLineEdit.text())
-
-        eoc_voltage = float(self.ui.eocVoltageLineEdit.text())
-        low_voltage_charge = float(self.ui.LowVoltageChargeLineEdit.text())
-
-        sleep_voltage = float(self.ui.sleepVoltageLineEdit.text())
-        
-        
-        # Convert the integer
-        ov_hex = int(ov / VOLTAGE_CELL_MULTIPLIER)
-        ov_recover_hex = int(ov_recover / VOLTAGE_CELL_MULTIPLIER)
-        
-        uv_hex = int(uv / VOLTAGE_CELL_MULTIPLIER)
-        uv_recover_hex = int(uv_recover / VOLTAGE_CELL_MULTIPLIER)
-
-        ov_lockout_hex = int(ov_lockout / VOLTAGE_CELL_MULTIPLIER)
-        uv_lockout_hex = int(uv_lockout / VOLTAGE_CELL_MULTIPLIER)
-
-        eoc_voltage_hex = int(eoc_voltage / VOLTAGE_CELL_MULTIPLIER)
-        low_voltage_charge_hex = int(low_voltage_charge / VOLTAGE_CELL_MULTIPLIER)
-        
-        sleep_voltage_hex = int(sleep_voltage / VOLTAGE_CELL_MULTIPLIER)
-        
-        self.bms_config.reg_write( 0x00, ov_hex, MASK_12BIT, 0x00)
-        self.bms_config.reg_write( 0x02, ov_recover_hex, MASK_12BIT, 0x00)
-        self.bms_config.reg_write( 0x04, uv_hex, MASK_12BIT, 0x00)
-        self.bms_config.reg_write( 0x06, uv_recover_hex, MASK_12BIT, 0x00)
-        self.bms_config.reg_write( 0x08, ov_lockout_hex, MASK_12BIT, 0x00)
-        self.bms_config.reg_write( 0x0a, uv_lockout_hex, MASK_12BIT, 0x00)
-        self.bms_config.reg_write( 0x0c, eoc_voltage_hex, MASK_12BIT, 0x00)
-        self.bms_config.reg_write( 0x0e, low_voltage_charge_hex, MASK_12BIT, 0x00)
-        self.bms_config.reg_write( 0x44, sleep_voltage_hex, MASK_12BIT, 0x00)
-        
-        #time register
-         # Get the values from the QLineEdit fields
-        ov_delay_timeout = int(self.ui.ovDelayTimeoutLineEdit.text()) #reg00
+    def write_time_registers(self):
+        # Extract values from QLineEdit fields
+        ov_delay_timeout = int(self.ui.ovDelayTimeoutLineEdit.text())
         uv_delay_timeout = int(self.ui.uvDelayTimeoutLineEdit.text())
-        
         open_wire_sample_time = int(self.ui.openWireTimingLineEdit.text())
-        sleep_delay = int(self.ui.sleepDelayLineEdit.text())        
-        
-        #Read unit and shift the values according to position in the register
+        sleep_delay = int(self.ui.sleepDelayLineEdit.text())
+    
+        # Extract and shift units
         ov_delay_timeout_unit = self.get_unit_from_combo(self.ui.ovDelayTimeoutCombo) << 10
         uv_delay_timeout_unit = self.get_unit_from_combo(self.ui.uvDelayTimeoutCombo) << 10
         sleep_delay_unit = self.get_unit_from_combo(self.ui.sleepDelayUnitCombo) << 9
         open_wire_sample_time_unit = self.get_unit_from_combo(self.ui.openWireTimingCombo) << 9
+    
+        # Combine values and units, then write to registers
+        self.bms_config.reg_write(0x10, ov_delay_timeout_unit | ov_delay_timeout, MASK_12BIT, 0)
+        self.bms_config.reg_write(0x12, uv_delay_timeout_unit | uv_delay_timeout, MASK_12BIT, 0)
+        self.bms_config.reg_write(0x14, open_wire_sample_time_unit | open_wire_sample_time, MASK_10BIT, 0)
+        self.bms_config.reg_write(0x46, sleep_delay_unit | sleep_delay, MASK_11BIT, 0)
 
-        #Joins values and units, and write the configuration
-        self.bms_config.reg_write( 0x10, ov_delay_timeout_unit|ov_delay_timeout, MASK_12BIT, 0)
-        self.bms_config.reg_write( 0x12, uv_delay_timeout_unit|uv_delay_timeout, MASK_12BIT, 0)
-        self.bms_config.reg_write( 0x14, open_wire_sample_time_unit |open_wire_sample_time, MASK_10BIT, 0)
-        self.bms_config.reg_write( 0x46, sleep_delay_unit | sleep_delay, MASK_11BIT, 0)  
-
-
-
-        register_cfg = self.bms_config.get_config()
-        print("exit write_bms_config:", register_cfg)
-
-        register_cfg_int = [int(val, 16) for val in register_cfg]
-
+    def send_serial_command(self, command, data):
         # Access the shared serial_setup
         serial_setup = self.ui.serial_setup
 
-        if serial_setup and serial_setup.is_open():
-            # Send the configuration data over serial
-            serial_protocol = SerialProtocol(serial_setup)
-            serial_protocol.send_command(CMD_WRITE_EEPROM, register_cfg_int)    
-        else:
-            print("Serial port is not open")
+        try:
+            if serial_setup and serial_setup.is_open():
+                # Send the configuration data over serial
+                serial_protocol = SerialProtocol(serial_setup)
+                serial_protocol.send_command(command, data)
+            else:
+                logging.warning("Serial port is not open")
+        except Exception as e:
+            logging.error(f"Failed to send serial command: {e}")
+
+    def write_bms_config(self):
+
+        register_cfg = self.bms_config.get_config()
+
+        print("entered write_bms_config:", register_cfg)       
+
+        self.write_voltage_registers()
+        self.write_time_registers()
+
+        register_cfg = self.bms_config.get_config()
+        print("exit write_bms_config:", register_cfg)
+        register_cfg_int = [int(val, 16) for val in register_cfg]
+
+        self.send_serial_command(CMD_WRITE_EEPROM, register_cfg_int)
+
+
  
-    def on_line_edit_changed(self, text):
-        print(f"Line Edit Changed: {text}")
