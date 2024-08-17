ec721f9002a02b823d4c23100ac31d773ef6f12f
Joao Paulo Bino <joao.nogueirabino@edu.fh-joanneum.at>
2024-08-17 16:57:34 +0200

    Add Roboto font to the GUI

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-
+import os
 import sys
 import logging
 
 from PySide6.QtUiTools import loadUiType
 from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtGui import QIcon
+from PySide6.QtGui import QIcon, QFont, QFontDatabase
 
 from config import WINDOW_TITLE, ICON_PATH, UI_FILE_PATH
 from bms.configuration import BMSConfiguration
@@ -14,7 +14,7 @@ from gui.serial import SerialWidget
 WINDOW_WIDTH = 1126
 WINDOW_HEIGHT = 885
 
-Ui_MainWindow, _ = loadUiType(UI_FILE_PATH) 
+Ui_MainWindow, _ = loadUiType(UI_FILE_PATH)
 
 def configure_logging():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -24,6 +24,24 @@ def main():
     logging.info("Application started")
     app = QApplication(sys.argv)
 
+    # Load and set Roboto font
+    root_directory = os.path.dirname(os.path.abspath(__file__))
+    font_path = os.path.join(root_directory, "../qt/fonts/Roboto/Roboto-Regular.ttf")
+
+    if os.path.exists(font_path):
+        print(f"Font file found at: {font_path}")
+        font_id = QFontDatabase.addApplicationFont(font_path)
+        if font_id != -1:
+            font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+            roboto_font = QFont(font_family)
+            roboto_font.setPointSize(9)
+            app.setFont(roboto_font)
+            print(f"Roboto font applied: {font_family}")
+        else:
+            print("Failed to load Roboto font")
+    else:
+        print(f"Font file not found at: {font_path}")
+
     try:
         fmcw_app = FMCWApplication(UI_FILE_PATH)
         fmcw_app.show()
@@ -32,7 +50,7 @@ def main():
         logging.error(f"Failed to start the application: {e}")
         sys.exit(1)
 
-class FMCWApplication(QMainWindow, Ui_MainWindow ):
+class FMCWApplication(QMainWindow, Ui_MainWindow):
     def __init__(self, ui_file_name):
         super().__init__()
         self.setupUi(self)
