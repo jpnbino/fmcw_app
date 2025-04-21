147282326ff47cb8f6b1bba7f03fb974de45abe7
Joao Paulo Bino <joao.nogueirabino@edu.fh-joanneum.at>
2025-04-21 19:07:52 +0200

    FMCW-39 Fix calculation for temperature related cell balance registers.

--- src/bms/cfg_cell_balance_registers.py
+++ src/bms/cfg_cell_balance_registers.py
@@ -5,30 +5,36 @@ from .cfg_timeout_registers import TimeRegisterField, TimeUnitRegisterField
 
 from .isl94203_constants import *
 
+
+def voltage_temperature_from_raw(value: int) -> float:
+    return value * TEMPERATURE_MULTIPLIER
+
+def voltage_temperature_to_raw(value: float) -> int:
+    return int(value / TEMPERATURE_MULTIPLIER)
 """Cell Balance Group """
+
 
 # Voltage Definitions
 v_map = [
-    ("cell_balance_min_voltage", 0x1C),
-    ("cell_balance_max_voltage", 0x1E),
-    ("cell_balance_max_delta", 0x20),
-    ("cell_balance_min_delta", 0x22),
-    ("cell_balance_min_temp", 0x28),
-    ("cell_balance_min_temp_recovery", 0x2A),
-    ("cell_balance_max_temp", 0x2C),
-    ("cell_balance_max_temp_recovery", 0x2E),
+    ("cell_balance_min_voltage", 0x1C, voltage_cell_from_raw, voltage_cell_to_raw),
+    ("cell_balance_max_voltage", 0x1E, voltage_cell_from_raw, voltage_cell_to_raw),
+    ("cell_balance_max_delta", 0x20, voltage_cell_from_raw, voltage_cell_to_raw),
+    ("cell_balance_min_delta", 0x22, voltage_cell_from_raw, voltage_cell_to_raw),
+    ("cell_balance_min_temp", 0x28, voltage_temperature_from_raw, voltage_temperature_to_raw),
+    ("cell_balance_min_temp_recovery", 0x2A, voltage_temperature_from_raw, voltage_temperature_to_raw),
+    ("cell_balance_max_temp", 0x2C, voltage_temperature_from_raw, voltage_temperature_to_raw),
+    ("cell_balance_max_temp_recovery", 0x2E, voltage_temperature_from_raw, voltage_temperature_to_raw),
 ]
 
-
 v_reg = {}
-for name, address in v_map:
+for name, address, from_raw, to_raw in v_map:
     v_reg[name] = VoltageRegisterField(
         name=name,
         address=address,
         bit_mask=Mask.MASK_12BIT,
         bit_position=0,
-        to_raw= voltage_cell_to_raw,
-        from_raw = voltage_cell_from_raw,
+        to_raw=to_raw,
+        from_raw=from_raw,
     )
 
 # Time Definitions
